474ab529cbc5ba4b616aeaa72cdd51f5c925e517
joshua-983 <officialgh983@gmail.com>
2024-04-11 15:38:41 -0700

    classes

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,18 +1,12 @@
 export default class Building {
   constructor(sqft) {
+    if (new.target !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
     this._sqft = sqft;
-    console.log(this.#evacuationWarningMessage())
   }
 
   get sqft() {
     return this._sqft;
   }
-
-  set sqft(sqft) {
-    this._sqft = sqft;
-  }
-
- #evacuationWarningMessage(){
-  return 'Hey There'
- }
 }
