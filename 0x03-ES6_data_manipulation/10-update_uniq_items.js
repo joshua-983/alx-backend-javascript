29129c3b6b72bb7d29d1a5845403cff05bc42273
joshua-983 <officialgh983@gmail.com>
2024-04-16 11:51:15 -0700

    data manipulation

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+function updateUniqueItems(map) {
+    if (!(map instanceof Map)) {
+        throw new Error('Cannot process');
+    }
+
+    for (let [item, quantity] of map.entries()) {
+        if (quantity === 1) {
+            map.set(item, 100);
+        }
+    }
+}
